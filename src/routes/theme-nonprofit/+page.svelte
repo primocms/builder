4448a0df19084f15e90d470042043d448aff06bc
Courtney Morris <courtney@primo.so>
2023-05-26 16:23:07 -0400

    add initial test

---
+++ src/routes/theme-nonprofit/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import PrimoPage from '$lib/views/editor/Page.svelte'
+
+	export let data
+</script>
+
+<PrimoPage {data} />
--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,9 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+test('Navigates to root-level page', async ({ page }) => {
+	await page.goto('http://localhost:5173/theme-nonprofit');
+	await page.click('button#toolbar--pages');
+	await page.getByRole('link', { name: 'About /about' }).click();
+	await page.getByRole('navigation', { name: 'toolbar' }).getByText('About')
+	await expect(page.url()).toBe('http://localhost:5173/theme-nonprofit/about');
 });
-
-// navigate between pages
-test('navigate between pages', async ({ page }) => {
-	await page.goto('/');
-	await page.click('text=about');
-
-  await page.getByRole('button', { name: 'Pages' }).click();
-  await page.getByRole('link', { name: 'About /' }).click();
-	await expect(page.url()).toBe('http://localhost:3000/about');
-	await page.goBack();
-	await expect(page.url()).toBe('http://localhost:3000/');
-})
