d220734b4a5d1aaa4800223f261e590244335e08
Mateo Morris <accounts@mateomorris.com>
2024-01-25 20:35:09 -0500

    download images in root-level sections

--- src/lib/views/modal/Deploy.svelte
+++ src/lib/views/modal/Deploy.svelte
@@ -58,7 +58,7 @@
 
 	async function download_site() {
 		loading = true
-		const files = await build_site_bundle({ pages: $pages, symbols: $symbols })
+		const files = await build_site_bundle({ pages: $pages, symbols: $symbols }, true)
 		if (!files) {
 			loading = false
 			return
@@ -69,8 +69,12 @@
 
 		async function create_site_zip(files) {
 			const zip = new JSZip()
-			files.forEach(({ path, content }) => {
-				zip.file(path, content)
+			files.forEach(({ path, content, blob }) => {
+				if (blob) {
+					zip.file(path, blob, { binary: true })
+				} else {
+					zip.file(path, content)
+				}
 			})
 			return await zip.generateAsync({ type: 'blob' })
 		}
